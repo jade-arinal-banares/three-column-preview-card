62a7185d5861aea4a191ef23f03b650fc9c703c0
Jade-Banares <83527821+jade-arinal-banares@users.noreply.github.com>
2022-12-25 22:54:31 +0800

    finish with the challenge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./*html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "375px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+      colors: {
+        brightOrange: "hsl(31, 77%, 52%)",
+        darkCyan: "hsl(184, 100%, 22%)",
+        veryDarkCyan: "hsl(179, 100%, 13%)",
+        bgColor: "hsl(0, 0%, 95%)",
+        txtColor: "hsla(0, 0%, 100%, 0.75)",
+      },
+      fontFamily: {
+        fontFam: ["Big Shoulders Display", "cursive"],
+      },
+    },
   },
   plugins: [],
 };
